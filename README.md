14b504d6e17e06e04b89be66c4a9960ad955cd74
martin <diaz.martin06@gmail.com>
2015-12-24 10:14:11 -0300

    basic servlet example

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ServletExample
 This is a simple jetty servlet example
+
+Requirements:
+1. Eclipse Luna 
+2. apache maven 3.3.2
+
+Instructions: 
+
+1. Clone the project to your workspace. 
+2. In eclipse do a maven import.
+
+
+Once the proyect has been imported you an run the com.amdocs.servlet.app.Application to start the listener. In the controller the methods are executed.
---
+++ src/main/java/com/amdocs/servlet/controller/ControllerExample.java
@@ -0,0 +1,45 @@
+package com.amdocs.servlet.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * 
+ * The <code>ControllerExample</code> class controls how access to the webApp will be obtained
+ * via the browser.
+ * Addresses is mapped to /localhost:8080/test/example/method1/var=123.
+ * version 1.0
+ * 
+ * @author  Martín Díaz
+ */
+@RestController
+@RequestMapping("/example")
+public class ControllerExample {
+
+    @RequestMapping(value = "/method1/{var}", method = RequestMethod.GET)     
+    @ResponseBody
+    public String listenerPathTest(@PathVariable String var) throws Exception{    	
+
+    	return "this is a test" + var;
+    }
+    
+    @RequestMapping(value = "/method2", method = RequestMethod.GET)     
+  	@ResponseBody
+  	public void listenerPathTest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+  
+    	System.out.println("Accesed method");
+    	response.setHeader("success", null);
+    	
+    }
+
+}
