31ce5d605956577bc8505add4bea7b71b07cb60f
Hiessy <diaz.martin06@gmail.com>
2017-03-03 11:11:40 -0300

    Refactoring whole proyecto to run on spring boot

--- src/main/java/com/hiessy/servlet/controller/ControllerExample.java
+++ src/main/java/com/hiessy/servlet/controller/ControllerExample.java
@@ -1,15 +1,11 @@
-package com.mdiaz.servlet.controller;
+package com.hiessy.servlet.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.ObjectWriter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/test/java/com/hiessy/servlet/app/AppTest.java
+++ src/test/java/com/hiessy/servlet/app/AppTest.java
@@ -1,4 +1,4 @@
-package com.amdocs.servlet.app;
+package com.hiessy.servlet.app;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
